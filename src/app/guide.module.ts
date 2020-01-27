cb56067e2f343cb87cd7333ff2502a5a4d846527
AgnieszkaKurek <agniechakurek@gmail.com>
2020-01-27 12:24:12 +0100

    add gride-view component

--- src/app/guide.module.ts
+++ src/app/guide.module.ts
@@ -2,11 +2,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { GuideComponent } from './guide/guide.component';
+import { GrideViewComponent } from './gride-view/gride-view.component';
 
 
 @NgModule({
   declarations: [
     GuideComponent,
+    GrideViewComponent,
   ],
   imports: [
     BrowserModule,
