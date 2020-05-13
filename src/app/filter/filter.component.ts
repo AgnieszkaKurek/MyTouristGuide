5c20f5306e35faed5771882c6cd15ba111a35fa0
AgnieszkaKurek <agniechakurek@gmail.com>
2020-05-13 10:55:55 +0200

    refactoring filtering of attractions

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,17 +1,34 @@
-import { Component, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'tg-filter',
   templateUrl: './filter.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit, OnDestroy {
 
-  @Output()
-  public filterChanged: EventEmitter<string> = new EventEmitter<string>();
+  private sub: Subscription;
 
-  public onKeyUp(event: Event): void {
-    const filterValue = (event.target as HTMLInputElement).value.trim();
-    this.filterChanged.emit(filterValue);
+  public form: FormGroup;
+
+  public ngOnInit(): void {
+    this.form = new FormGroup({
+      filter: new FormControl(),
+    });
+    this.sub = this.form.get('filter').valueChanges.pipe(
+      distinctUntilChanged(),
+      debounceTime(300),
+    ).subscribe(filter => {
+      console.log(`filter changed to: ${filter}`);
+    })
+  }
+
+  public ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 }
