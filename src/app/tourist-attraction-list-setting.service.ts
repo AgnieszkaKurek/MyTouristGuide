a8c73fee610c08e2709c3eef41cfc45c1f4320b5
AgnieszkaKurek <agniechakurek@gmail.com>
2020-07-30 17:26:17 +0200

    removing syntax errors

--- src/app/tourist-attraction-list-setting.service.ts
+++ src/app/tourist-attraction-list-setting.service.ts
@@ -10,20 +10,20 @@ export interface TouristAttractionListSetting {
 })
 export class TouristAttractionListSettingService {
 
-  private _setting: TouristAttractionListSetting = {
+  private setting: TouristAttractionListSetting = {
     filter: '',
-  }
-  private _settingsChangedSource: BehaviorSubject<TouristAttractionListSetting> =
-    new BehaviorSubject<TouristAttractionListSetting>(this._setting);
+  };
+  private settingsChangedSource: BehaviorSubject<TouristAttractionListSetting> =
+    new BehaviorSubject<TouristAttractionListSetting>(this.setting);
 
-  public settingsChanged: Observable<TouristAttractionListSetting> = this._settingsChangedSource.asObservable();
+  public settingsChanged: Observable<TouristAttractionListSetting> = this.settingsChangedSource.asObservable();
 
   public set filter(filter: string) {
-    this._setting = {
-      ...this._setting,
+    this.setting = {
+      ...this.setting,
       filter: filter.toLocaleLowerCase(),
     };
-    this._settingsChangedSource.next(this._setting);
-  };
+    this.settingsChangedSource.next(this.setting);
+  }
 
 }
