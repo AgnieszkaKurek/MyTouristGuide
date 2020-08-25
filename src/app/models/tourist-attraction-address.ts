c85da9199ed7d60ded0b9eb2941bceb421df0cc1
AgnieszkaKurek <agniechakurek@gmail.com>
2020-08-25 13:03:03 +0200

    add more information abaut tourist attraction

---
+++ src/app/models/tourist-attraction-address.ts
@@ -0,0 +1,4 @@
+export interface TouristAttractionAddress {
+  street: string;
+  zipCode: string;
+}
--- src/app/models/tourist-attraction.ts
+++ src/app/models/tourist-attraction.ts
@@ -1,8 +1,13 @@
 import { TouristAttractionCategory } from './tourist-attraction-category.enum';
+import { TouristAttractionAddress } from './tourist-attraction-address';
 
 export interface TouristAttraction {
   category: TouristAttractionCategory;
   name: string;
+  website: string;
   place: string;
+  address: TouristAttractionAddress;
+  voivodeship: string;
+  euroregion: string;
   lastVisited: Date;
 }
