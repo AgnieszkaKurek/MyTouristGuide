3a7a01a2f93f1fd84c5055defd1d826b8c2b71cd
AgnieszkaKurek <agniechakurek@gmail.com>
2020-09-22 11:54:00 +0200

    moving components to new folders

--- src/app/guide.module.ts
+++ src/app/guide.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { GuideComponent } from './guide/guide.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TileListComponent } from './tile-list/tile-list.component';
-import { MapComponent } from './map/map.component';
+import { GuideComponent } from './core/guide/guide.component';
+import { TableListComponent } from './table-list/table-list/table-list.component';
+import { TileListComponent } from './tile-list/tile-list/tile-list.component';
+import { MapComponent } from './map/map/map.component';
 import { GuideRoutingModule } from './guide-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -11,18 +11,18 @@ import { TouristAttractionDb } from './tourist-attraction-db.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
-import { TableListGridComponent } from './table-list-grid/table-list-grid.component';
-import { FilterComponent } from './filter/filter.component';
+import { TableListGridComponent } from './table-list/table-list-grid/table-list-grid.component';
+import { FilterComponent } from './shared/filter/filter.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PaginatorComponent } from './paginator/paginator.component';
+import { PaginatorComponent } from './shared/paginator/paginator.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { TouristAttractionDetailComponent } from './tourist-attraction-detail/tourist-attraction-detail.component';
+import { TouristAttractionDetailComponent } from './tourist-attraction-detail/tourist-attraction-detail/tourist-attraction-detail.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
-import { TouristAttractionAddressComponent } from './tourist-attraction-address/tourist-attraction-address.component';
+import { TouristAttractionAddressComponent } from './tourist-attraction-detail/tourist-attraction-address/tourist-attraction-address.component';
 
 @NgModule({
   declarations: [
