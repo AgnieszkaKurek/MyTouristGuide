963d5e18680735a21a363b3fd6ff151fed1ca82a
AgnieszkaKurek <agniechakurek@gmail.com>
2020-10-05 17:25:50 +0200

    Tile view part 1

---
+++ src/app/tile/tile-grid/tile-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'tg-tile-grid',
+  templateUrl: './tile-grid.component.html',
+  styleUrls: ['./tile-grid.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TileGrideComponent {//todo - wrong name
+
+}
