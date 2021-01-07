18cd67ec0d4d4e614872e4d9e4b46eec5f04c993
AgnieszkaKurek <agniechakurek@gmail.com>
2021-01-07 17:01:48 +0100

    adding a reset form for adding a new attraction

--- src/app/attraction-edit/attraction-edit/attraction-edit.component.ts
+++ src/app/attraction-edit/attraction-edit/attraction-edit.component.ts
@@ -26,6 +26,7 @@ export class AttractionEditComponent implements OnDestroy {
     const attraction = this.form.getRawValue() as TouristAttraction;
     this._addActionSubscription = this._attractionEditService.add(attraction).subscribe();
     this._router.navigate(['list']);
+    this.form.reset();
   }
 
   public ngOnDestroy(): void {
